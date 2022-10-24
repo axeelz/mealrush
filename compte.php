d67fb75c94710b75e15a8af2696bc2c0731aac3b
axelzareb <axelzareb@gmail.com>
2022-10-24 14:08:09 +0200

    modif page accueil afficher tags et restos

--- compte.php
+++ compte.php
@@ -495,9 +495,14 @@ if (isset($_POST['setadress']) && isset($conn)) {
 
     <div class="modal" id="ouvrir-adresses">
         <div class="modal-box">
-            <h3 class="font-bold text-lg">Adresses enregistrées</h3>
+            <h3 class="font-bold text-lg mb-2">Adresses enregistrées</h3>
             <?php foreach ($adresses as $a) : ?>
-                <p class="py-4"><?php echo $a; ?></p>
+                <button class="btn btn-ghost gap-2">
+                    <?php echo $a; ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M 18 2 L 15.585938 4.4140625 L 19.585938 8.4140625 L 22 6 L 18 2 z M 14.076172 5.9238281 L 3 17 L 3 21 L 7 21 L 18.076172 9.9238281 L 14.076172 5.9238281 z" />
+                    </svg>
+                </button>
             <?php endforeach; ?>
             <div class="modal-action">
                 <form method="post" action="compte.php">
--- index.php
+++ index.php
@@ -13,6 +13,18 @@ try {
     while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
         array_push($tags, $row["nom_tag"] . ":" . $row["id"]);
     }
+
+    // On récupère les restaurants pour les afficher
+    $restos = array();
+    $query = "SELECT * FROM restaurants WHERE approuve = 'true'";
+    $result = mysqli_query($conn, $query);
+    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        array_push($restos, array(
+            'nom' => $row['nom'],
+            'image' => $row['image'],
+            'id' => $row['id']
+        ));
+    }
 } catch (\Throwable $th) {
     array_push($erreurs, $th->getMessage());
 }
@@ -36,6 +48,32 @@ try {
     <!-- Navigation -->
     <?php include('navbar.php'); ?>
 
+    <!-- <div class="flex items-center p-4 md:w-1/3 mx-auto">
+        <div class="flex-1 px-2">
+            <h2 class="text-xl font-extrabold">Livraison à</h2>
+            <p class="text-md text-opacity-80">5 rue de Rivoli</p>
+        </div>
+        <div class="flex-0">
+            <div class="dropdown dropdown-top dropdown-end">
+                <div tabindex="0">
+                    <div class="flex space-x-1">
+                        <button aria-label="button component" class="btn btn-ghost btn-square">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-6 w-6 stroke-current">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+
+    <div class="flex first:ml-auto last:mr-auto items-center p-4 max-w-100 bg-base-300 overflow-x-auto justify-around" id="tags-container">
+        <?php foreach ($tags as $t) : ?>
+            <a class="btn btn-ghost"><?php echo explode(":", $t)[0]; ?></a>
+        <?php endforeach; ?>
+    </div>
+
     <div class="hero min-h-fit" style="background-image: url(img/main-hero.jpeg);">
         <div class="hero-overlay bg-opacity-70"></div>
         <div class="hero-content text-center text-neutral-content flex-col">
@@ -51,8 +89,28 @@ try {
         </div>
     </div>
 
-    <!-- Livrer a ...
-    Lister les catégories -->
+    <div class="grid grid-cols-3 gap-4 p-5" id="tags-container">
+        <?php foreach ($restos as $r) : ?>
+            <div class="card w-96 bg-base-100 shadow-xl max-w-full">
+                <figure class="max-h-36 overflow-hidden">
+                    <img src="<?php echo $r['image']; ?>" alt="Image Restaurant" class="object-cover" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">
+                        <?php echo $r['nom']; ?>
+                        <div class="badge badge-secondary">Nouveau</div>
+                    </h2>
+                    <div class="card-actions justify-start">
+                        <div class="badge badge-outline">Kebab</div>
+                        <div class="badge badge-outline">Pizza</div>
+                    </div>
+                    <div class="card-actions justify-end">
+                        <button class="btn bg-blue text-black border-none hover:text-white">Voir les plats</button>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
 
     <?php include('footer.php'); ?>
 
