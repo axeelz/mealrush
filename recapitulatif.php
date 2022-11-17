09a1b4712eb97451973212921633de1963c3c787
axeelz <axelzareb@hotmail.fr>
2022-11-17 23:58:11 +0100

    opti exec

--- recapitulatif.php
+++ recapitulatif.php
@@ -97,7 +97,9 @@ if (isset($_POST['payer']) && isset($conn)) {
                             $contenu_mail .= nl2br($i['quantite'] . " " . $i['nom'] . " pour " . str_replace('.', ',', $i['prix'] * $i['quantite']) . "€\n");
                         }
                         $contenu_mail .= nl2br("\nTotal avec charges : " . str_replace('.', ',', $_SESSION['panier']['prix_final']) . "€");
-                        exec("/Applications/XAMPP/xamppfiles/bin/php " . realpath("email.php") . " '" . $sujet_mail . "' '" . $contenu_mail . "' '" . $destinataire . "' 2>&1 &", $output);
+
+                        // On utilise exec pour effectuer la tache en arrière plan afin de ne pas bloquer le chargement de la page pour l'utilisateur
+                        exec(PHP_BINDIR . "/php " . realpath("email.php") . " '" . $sujet_mail . "' '" . $contenu_mail . "' '" . $destinataire . "' 2>&1 &", $output);
                     } catch (\Throwable $th) {
                         echo $th;
                     }
