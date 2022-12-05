ad1cf2837ed9b03701d775a3e897b7a1f1a4b580
axeelz <axelzareb@hotmail.fr>
2022-12-05 17:22:59 +0100

    z-index sur notifications

--- components/footer.php
+++ components/footer.php
@@ -10,7 +10,7 @@
 
     <div class="toast toast-end">
         <?php foreach ($erreurs as $v) : ?>
-            <div class="temp alert alert-error shadow-lg">
+            <div class="temp alert alert-error shadow-lg z-50">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -20,7 +20,7 @@
             </div>
         <?php endforeach; ?>
         <?php foreach ($succes as $s) : ?>
-            <div class="temp alert alert-success shadow-lg">
+            <div class="temp alert alert-success shadow-lg z-50">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -31,7 +31,7 @@
         <?php endforeach; ?>
         <!-- Afficher message provenant d'une autre page -->
         <?php if (isset($_SESSION['successMessage'])) : ?>
-            <div class="temp alert alert-success shadow-lg">
+            <div class="temp alert alert-success shadow-lg z-50">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -43,7 +43,7 @@
         <?php endif; ?>
         <!-- Afficher message à la déconnexion -->
         <?php if (!empty($_GET['deconnexion'])) : ?>
-            <div class="temp alert alert-success shadow-lg">
+            <div class="temp alert alert-success shadow-lg z-50">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
